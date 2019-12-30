72bf84800c2a4fd0d9dd67c0eaf71d30b7671175
EVA140520 <1097134948@qq.com>
2019-12-31 00:57:59 +0800

    添加依赖

--- app/src/main/java/org/eva/jason140520/MainActivity.kt
+++ app/src/main/java/org/eva/jason140520/MainActivity.kt
@@ -2,9 +2,18 @@ package org.eva.jason140520
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/**
+  * @ClassName:      MainActivity
+  * @Description:     java类作用描述 
+  * @Author:         JiangLin
+  * @CreateDate:     2019/12/30 23:39
+  * @UpdateUser:     更新者
+  * @UpdateDate:     2019/12/30 23:39
+  * @UpdateRemark:   更新说明
+  * @Version:        1.0
+ */
 class MainActivity : AppCompatActivity() {
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
