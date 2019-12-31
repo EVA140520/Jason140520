e924d71226ed9448094c77663791d61e053e4fcb
1097134948@qq.com <1097134948@qq.com>
2019-12-31 17:41:21 +0800

    添加启动页面

---
+++ app/src/main/java/org/eva/jason140520/view/LaunchActivity.kt
@@ -0,0 +1,23 @@
+package org.eva.jason140520.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.eva.jason140520.R
+
+/**
+ * @ClassName:      MainActivity
+ * @Description:     java类作用描述
+ * @Author:         JiangLin
+ * @CreateDate:     2019/12/30 23:39
+ * @UpdateUser:     JiangLin
+ * @UpdateDate:     2019/12/30 23:39
+ * @UpdateRemark:   新建类
+ * @Version:        1.0
+ */
+class LaunchActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
