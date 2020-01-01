bf7b77707fcb72444e7cc08a31d8526e2b204cf1
EVA140520 <1097134948@qq.com>
2020-01-01 22:10:19 +0800

    添加方法

---
+++ app/src/main/java/org/eva/jason140520/view/TypeFragment.kt
@@ -0,0 +1,22 @@
+package org.eva.jason140520.view
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Copyright (C), 2020, Jason
+ * Author: JiangLin
+ * Date: 2020/1/1 21:51
+ * History:
+ * <author> <time> <version> <desc>
+ * 作者姓名 修改时间 版本号 描述
+ */
+
+/**
+ * @ClassName: TypeFragmetn
+ * @Description: 分类Fragment
+ * @Author: JiangLin
+ * @Date: 2020/1/1 21:51
+ */
+class TypeFragment:Fragment() {
+    ViewPager2
+}
